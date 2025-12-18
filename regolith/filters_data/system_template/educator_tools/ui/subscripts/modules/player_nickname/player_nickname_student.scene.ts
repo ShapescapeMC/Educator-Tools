3f569bdcb144cd37810bc1c5de961144073bc1d2
Fabrizio La Rosa <fabrizio.larosa@shapescape.com>
2025-12-18 15:22:07 +0100

    Add student nickname scene for educator tools UI
    Introduces PlayerNicknameStudentScene to allow students to set or clear their nickname via a modal UI. Integrates with PlayerNicknameService and updates nickname based on user input.

---
+++ regolith/filters_data/system_template/educator_tools/ui/subscripts/modules/player_nickname/player_nickname_student.scene.ts
@@ -0,0 +1,47 @@
+import { SceneContext } from "../scene_manager/scene-context";
+import { SceneManager } from "../scene_manager/scene-manager";
+import { ModalUIScene } from "../scene_manager/ui-scene";
+import { PlayerNicknameService } from "./player_nickname.service";
+
+export class PlayerNicknameStudentScene extends ModalUIScene {
+	static readonly id = "player_nickname_student";
+
+	constructor(
+		sceneManager: SceneManager,
+		context: SceneContext,
+		private readonly playerNicknameService: PlayerNicknameService,
+	) {
+		super(PlayerNicknameStudentScene.id, context.getSourcePlayer());
+		this.setContext(context);
+
+		const currentNickname = this.playerNicknameService.getNickname(
+			context.getSourcePlayer().id,
+		);
+
+		this.addLabel({ translate: "edu_tools.ui.player_nickname_student.body" });
+
+		this.addTextField(
+			{ translate: "edu_tools.ui.player_nickname_student.nickname" },
+			currentNickname ? currentNickname : "John",
+			(value: string) => {
+				context.setData("nickname", value);
+			},
+		);
+
+		const response = this.show(context.getSourcePlayer(), sceneManager);
+		response.then((r) => {
+			if (r.canceled) {
+				return;
+			}
+			const nickname = context.getData("nickname") as string;
+			if (nickname && nickname.trim().length > 0) {
+				this.playerNicknameService.setNickname(
+					context.getSourcePlayer().id,
+					nickname.trim(),
+				);
+			} else {
+				this.playerNicknameService.clearNickname(context.getSourcePlayer().id);
+			}
+		});
+	}
+}
