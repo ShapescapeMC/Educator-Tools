c140e6c81e5378f0a1d0676105ba8bacc3f71730
Fabrizio La Rosa <fabrizio.larosa@shapescape.com>
2025-12-18 15:32:48 +0100

    Add PlayerNicknameSettingsScene for nickname settings UI
    Introduces PlayerNicknameSettingsScene, a modal UI scene for managing player nickname settings. Allows toggling 'prompt on join' and 'allow custom colors' options, and updates settings via PlayerNicknameService.

---
+++ regolith/filters_data/system_template/educator_tools/ui/subscripts/modules/player_nickname/player_nickname_settings.scene.ts
@@ -0,0 +1,59 @@
+import { SceneContext } from "../scene_manager/scene-context";
+import { SceneManager } from "../scene_manager/scene-manager";
+import { ModalUIScene } from "../scene_manager/ui-scene";
+import { PlayerNicknameService } from "./player_nickname.service";
+
+export class PlayerNicknameSettingsScene extends ModalUIScene {
+	static readonly id = "player_nickname_Settings";
+
+	constructor(
+		sceneManager: SceneManager,
+		context: SceneContext,
+		private readonly playerNicknameService: PlayerNicknameService,
+	) {
+		super(PlayerNicknameSettingsScene.id, context.getSourcePlayer());
+		this.setContext(context);
+
+		const settings = this.playerNicknameService.getSettings();
+
+		this.addLabel({ translate: "edu_tools.ui.player_nickname_student.body" });
+
+		this.addToggle(
+			"edu_tools.ui.player_nickname_settings.prompt_on_join",
+			(value: boolean) => {
+				context.setData("promptOnJoin", value);
+			},
+			{
+				defaultValue: settings.promptOnJoin,
+				tooltip: "edu_tools.ui.player_nickname_settings.prompt_on_join_tooltip",
+			},
+		);
+
+		this.addToggle(
+			"edu_tools.ui.player_nickname_settings.allow_custom_colors",
+			(value: boolean) => {
+				context.setData("allowCustomColors", value);
+			},
+			{
+				defaultValue: settings.allowCustomColors,
+				tooltip:
+					"edu_tools.ui.player_nickname_settings.allow_custom_colors_tooltip",
+			},
+		);
+
+		const response = this.show(context.getSourcePlayer(), sceneManager);
+		response.then((r) => {
+			if (r.canceled) {
+				return;
+			}
+
+			const promptOnJoin = context.getData("promptOnJoin") as boolean;
+			const allowCustomColors = context.getData("allowCustomColors") as boolean;
+
+			this.playerNicknameService.updateSettings({
+				promptOnJoin,
+				allowCustomColors,
+			});
+		});
+	}
+}
